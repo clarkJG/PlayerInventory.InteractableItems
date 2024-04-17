a7aed27f9f291a278d3da21921dab610948624b3
Jaz C <jxc220034@utdallas.edu>
2024-04-17 02:41:11 -0500

    GameState + Color Customization
    Can choose if game pauses when inventory is open. Can change color of inventory UI. Need to figure out key customization for opening inventory.

--- Assets/Inventory.Interactable/Scripts/InventoryManager.cs
+++ Assets/Inventory.Interactable/Scripts/InventoryManager.cs
@@ -2,33 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEditor;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
-    // Opens Inventory when key is pressed
-    //Pauses game when inventory is open
-
     public GameObject _inventoryMenu;
     private bool _menuOpen;
-    void Start()
+    public Image _imageMenu;
+    public Image _imageInventorySlots;
+    public Image _imageItemSlot;
+    public Image _imageInventoryDescription;
+    public Image _imageItemImage;
+    public Image _imageItemDescription;
+
+    public enum GameState
     {
-        
+        Paused = 0,
+        Running = 1
     }
+
+    [Header("Customizable Features")]
+    [SerializeField]
+    [Tooltip("Is the game paused or running in the background when inventory is opened")]
+    private GameState _gameState;
+    [SerializeField]
+    [Tooltip("Which key should be pressed to open the inventory")]
+    public KeyCode _openInventory = KeyCode.I;
+    //InvalidOperationException: You are trying to read Input using the UnityEngine.Input class, but you have switched active Input handling to Input System package in Player Settings.
+    //UnityEngine.Input.GetKey(UnityEngine.KeyCode key
+   
+
+       [Header("UI Colors")]
+    [SerializeField]
+    [ColorUsage(true)]
+    private Color _menu;
+    [SerializeField]
+    [ColorUsage(true)]
+    private Color _inventorySlots;
+    [SerializeField]
+    [ColorUsage(true)]
+    private Color _itemSlot;
+    [SerializeField]
+    [ColorUsage(true)]
+    private Color _inventoryDescription;
+    [SerializeField]
+    [ColorUsage(true)]
+    private Color _itemImage;
+    [SerializeField]
+    [ColorUsage(true)]
+    private Color _itemDescription;
 
 
     void Update()
     {
-        if ((Keyboard.current.iKey.wasPressedThisFrame) && _menuOpen)
+        // Opens Inventory when key is pressed
+        //Pauses game when inventory is open
+        if ((Input.GetKey(_openInventory)) && _menuOpen)
         {
             Time.timeScale = 1;
             _inventoryMenu.SetActive(false);
             _menuOpen = false;
         }
-        else if ((Keyboard.current.iKey.wasPressedThisFrame) && !_menuOpen)
+        else if ((Input.GetKey(_openInventory)) && !_menuOpen)
         {
-            Time.timeScale = 0;
+            Time.timeScale = (float)_gameState;
             _inventoryMenu.SetActive(true);
             _menuOpen = true;
         }
+
+        UIColor();
+    }
+
+    [SerializeField]
+    public void UIColor()
+    {
+        _imageMenu.color = _menu;
+        _imageInventorySlots.color = _inventorySlots;
+        _imageItemSlot.color = _itemSlot;
+        _imageInventoryDescription.color = _inventoryDescription;
+        _imageItemImage.color = _itemImage;
+        _imageItemDescription.color = _itemDescription;
     }
 }
